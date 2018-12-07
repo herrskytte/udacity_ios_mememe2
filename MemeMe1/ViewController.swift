fbe76fc20b42b85b0f16f317c92b691c1ebc6045
Frederik Skytte <fsk@computas.com>
2018-12-07 13:55:15 +0100

    Initial Commit

---
+++ MemeMe1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MemeMe1
+//
+//  Created by Frederik Skytte on 07/12/2018.
+//  Copyright © 2018 Frederik Skytte. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
